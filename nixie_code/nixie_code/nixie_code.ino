731fc44f4428e3e6f6d97539eee20fe06866a24a
Alex Hall <analytics@alex-hall.co.uk>
2020-11-18 20:19:08 +0000

    added PWM dimming

--- nixie_code/nixie_code/nixie_code.ino
+++ nixie_code/nixie_code/nixie_code.ino
@@ -16,7 +16,8 @@ int minButton = 5;
 
 
 int MULTIPLEX_DELAY = 2; //pulse width 2
-int BLANKING_INTERVAL = 200; //microseconds 200
+int BLANKING_INTERVAL_OPTIONS[5] = {200,400,800,1600,2400};
+int BLANKING_INTERVAL = BLANKING_INTERVAL_OPTIONS[0]; //microseconds 200
 const long CYCLE_INTERVAL = 1000; //interval between changes for cathode poisoning prevention scheme
 int NUM = 0; //start digit for cathode poisoning prevention
 unsigned long previousMillis = 0;
@@ -78,7 +79,6 @@ int minButtonState;
 
 void loop() {
 
- 
  DateTime tm = rtc.now();
  hr = tm.hour();
  mn = tm.minute();
@@ -90,50 +90,8 @@ void loop() {
   }
 
   else{
-  //get decimal values for each tube
-  
-  T1 = (hr / 10) % 10;
-  T2 = hr % 10;
-  T3 = (mn / 10) % 10;
-  T4 = mn % 10;
-  T5 = (sec / 10) % 10;
-  T6 = sec % 10;
-  
-
-
-      displayDigit(T1);
-      digitalWrite(11, HIGH);
-      delay(MULTIPLEX_DELAY);
-      digitalWrite(11,LOW);
-      delayMicroseconds(BLANKING_INTERVAL);
-      displayDigit(T2);
-      digitalWrite(10, HIGH);
-      delay(MULTIPLEX_DELAY);
-      digitalWrite(10,LOW);
-      delayMicroseconds(BLANKING_INTERVAL);
-      displayDigit(T3);
-      digitalWrite(9, HIGH);
-      delay(MULTIPLEX_DELAY);
-      digitalWrite(9,LOW);
-      delayMicroseconds(BLANKING_INTERVAL);
-      displayDigit(T4);
-      digitalWrite(8, HIGH);
-      delay(MULTIPLEX_DELAY);
-      digitalWrite(8,LOW);
-      delayMicroseconds(BLANKING_INTERVAL);
-      displayDigit(T5);
-      digitalWrite(7, HIGH);
-      delay(MULTIPLEX_DELAY);
-      digitalWrite(7,LOW);
-      delayMicroseconds(BLANKING_INTERVAL);
-      displayDigit(T6);
-      digitalWrite(6, HIGH);
-      delay(MULTIPLEX_DELAY);
-      digitalWrite(6,LOW);
-      delayMicroseconds(BLANKING_INTERVAL);
-
+  displayTime();
   }
-
 
   //handle button pushes
   //increment on button release so second zero doesnt affect hrs and mins
@@ -150,9 +108,50 @@ void loop() {
   }
 
   if(minButtonState == LOW and hrButtonState ==LOW){
-    zeroSeconds();
     delay(1000);
+    hrButtonState = digitalRead(hrButton);
+    minButtonState = digitalRead(minButton);
+    if(minButtonState == HIGH or hrButtonState ==HIGH){
+
+    zeroSeconds();
+    }
+  
+
+    
+    else{
+
+      int x=1;
+
+
+      //if both buttons held, change brightness
+      while(minButtonState == LOW and hrButtonState ==LOW){
+
+        displayTime();
+        
+        if(x>4){
+          x = 0;
+        }
+        
+        
+        unsigned long currentMillis = millis();
+        if(currentMillis - previousMillis >= CYCLE_INTERVAL){
+        previousMillis = currentMillis;
+        BLANKING_INTERVAL = BLANKING_INTERVAL_OPTIONS[x];
+        x=x+1;
+        }
+
+        hrButtonState = digitalRead(hrButton);
+        minButtonState = digitalRead(minButton);
+      }
+
+      // allow time to release both buttons
+      delay(1000);
+
+      previousMillis = 0;
+      
+    }
   }
+  
 
 
   previousHrButton = digitalRead(hrButton);
@@ -248,6 +247,54 @@ void displayDigit(int i){
 }
 
 
+void displayTime(){
+
+  //get decimal values for each tube
+  
+  T1 = (hr / 10) % 10;
+  T2 = hr % 10;
+  T3 = (mn / 10) % 10;
+  T4 = mn % 10;
+  T5 = (sec / 10) % 10;
+  T6 = sec % 10;
+  
+
+
+      displayDigit(T1);
+      digitalWrite(11, HIGH);
+      delay(MULTIPLEX_DELAY);
+      digitalWrite(11,LOW);
+      delayMicroseconds(BLANKING_INTERVAL);
+      displayDigit(T2);
+      digitalWrite(10, HIGH);
+      delay(MULTIPLEX_DELAY);
+      digitalWrite(10,LOW);
+      delayMicroseconds(BLANKING_INTERVAL);
+      displayDigit(T3);
+      digitalWrite(9, HIGH);
+      delay(MULTIPLEX_DELAY);
+      digitalWrite(9,LOW);
+      delayMicroseconds(BLANKING_INTERVAL);
+      displayDigit(T4);
+      digitalWrite(8, HIGH);
+      delay(MULTIPLEX_DELAY);
+      digitalWrite(8,LOW);
+      delayMicroseconds(BLANKING_INTERVAL);
+      displayDigit(T5);
+      digitalWrite(7, HIGH);
+      delay(MULTIPLEX_DELAY);
+      digitalWrite(7,LOW);
+      delayMicroseconds(BLANKING_INTERVAL);
+      displayDigit(T6);
+      digitalWrite(6, HIGH);
+      delay(MULTIPLEX_DELAY);
+      digitalWrite(6,LOW);
+      delayMicroseconds(BLANKING_INTERVAL);
+
+ 
+}
+
+
 
 void incrementHour(){
 
@@ -297,6 +344,7 @@ void zeroSeconds(){
 
 //cycle all digits to mitigate cathode poisoning
 void antiPoison(){
+  
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= CYCLE_INTERVAL) {
